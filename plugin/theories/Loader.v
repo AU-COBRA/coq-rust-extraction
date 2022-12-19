a04a045cea73ba660c4561b7bdb9c5a39a91b879
4ever2 <3417013+4ever2@users.noreply.github.com>
2022-12-19 15:47:05 +0100

    Setup repo (#1)

---
+++ plugin/theories/Loader.v
@@ -0,0 +1,6 @@
+From Coq Require Extraction.
+From MetaCoq.Template Require ExtractableLoader.
+
+(* Declare ML Module "extraction_plugin". *)
+(* Declare ML Module "coq-metacoq-template-coq.plugin". *)
+Declare ML Module "coq-concert-extraction.plugin".

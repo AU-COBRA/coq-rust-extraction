18e18e58e49c8280a65313e7a464c0b7c502c5eb
4ever2 <3417013+4ever2@users.noreply.github.com>
2025-01-29 01:14:53 +0100

    Bump MetaCoq version

--- .nix/coq-overlays/RustExtraction/default.nix
+++ .nix/coq-overlays/RustExtraction/default.nix
@@ -9,7 +9,7 @@ with lib; mkCoqDerivation {
 
   inherit version;
   defaultVersion = with versions; switch [coq.coq-version metacoq.version] [
-    { cases = [(range "8.17" "8.19") (range "1.3.1" "1.3.2")]; out = "0.1.0"; }
+    { cases = [(range "8.17" "8.19") (range "1.3.1" "1.3.3")]; out = "0.1.0"; }
   ] null;
 
   release."0.1.0".sha256 = "+Of/DP2Vjsa7ASKswjlvqqhcmDhC9WrozridedNZQkY=";
